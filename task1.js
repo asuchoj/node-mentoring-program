30f1a84884eecc66bffc6dcfa7813db71eec0886
Sharstsiuk, Aliaksei <aliaksei_sharstsiuk@epam.com>
2019-12-04 01:31:09 +0300

    basics.node completed 2 tasks

---
+++ task1.js
@@ -0,0 +1,11 @@
+const process = require('process');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  let chank;
+
+  while((chank = process.stdin.read()) !== null ) {
+    process.stdout.write([...chank].reverse().join(''));
+  }
+});
---
+++ task2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');  
+const {pipeline} = require('stream');
+const csvToJsonV2 = require("csvtojson");
+const process = require('process');
+const csvFilePath ='./csvdirectory/example.csv';
+const jsonFilePath ='./jsondirectory/test';
+const csvLowercaseColumnHeaders = ['book','author', 'amount', 'price'];
+const csvIgnoreColumnsColumnsReg = /(amount)/;
+
+async function writeConvertibleFileFromCsvToJSON(pathCsvFile, pathJsonFile, csvLowercaseColumnHeaders, csvIgnoreColumnsColumnsReg) {
+    await pipeline(
+        fs.createReadStream(pathCsvFile),
+        csvToJsonV2({
+            noheader: false,
+            headers: csvLowercaseColumnHeaders,
+            ignoreColumns: csvIgnoreColumnsColumnsReg,
+        }),
+        fs.createWriteStream(pathJsonFile),
+        (err) => {
+            if (err) {
+                logger(err);
+            }
+
+            logger('done');
+        }
+    )
+}
+
+writeConvertibleFileFromCsvToJSON(csvFilePath, jsonFilePath, csvLowercaseColumnHeaders, csvIgnoreColumnsColumnsReg).catch(err => {
+    logger(err);
+});
+
+function logger(message) {
+    console.log(message);
+}
