7a04a8a7e1d9bd349967cbdb09a0f62ce4a128c7
Sharstsiuk, Aliaksei <aliaksei_sharstsiuk@epam.com>
2020-01-20 22:33:50 +0300

    implement all task 3.1

--- task2/models/user.models.ts
+++ task2/models/user.models.ts
@@ -1,7 +1,7 @@
 import { User } from './../interfaces/user.interface';
 
 import {generatedUUIDv4} from "../utils/uuidGenerator";
-import { Sequelize, Model, INTEGER, STRING, BOOLEAN } from 'sequelize';
+import { Sequelize, INTEGER, STRING, BOOLEAN, Op } from 'sequelize';
 
 class UserModels {
   private DataBaseName: string = 'db';
@@ -11,51 +11,6 @@ class UserModels {
   private sequelize;
 
   public User;
-
-  users: User[] = [
-    {
-      login: "asdasdasalex",
-      password: "1111",
-      age: 12,
-      id: "693b9e13-1913-4115-a238-dc2a4571b221",
-      isDeleted: false
-    },
-    {
-      login: "aleandry",
-      password: "4444",
-      age: 13,
-      id: "693b9e13-1913-4115-a238-dc2a4571b222",
-      isDeleted: false
-    },
-    {
-      login: "Dimaale",
-      password: "2222",
-      age: 14,
-      id: "693b9e13-1913-4115-a238-dc2a4571b223",
-      isDeleted: false
-    },
-    {
-      login: "aleKolia",
-      password: "3333",
-      age: 15,
-      id: "693b9e13-1913-4115-a238-dc2a4571b224",
-      isDeleted: false
-    },
-    {
-      login: "aleVlad",
-      password: "4321",
-      age: 16,
-      id: "693b9e13-1913-4115-a238-dc2a4571b226",
-      isDeleted: false
-    },
-    {
-      login: "Sergei",
-      password: "1234",
-      age: 27,
-      id: "693b9e13-1913-4115-a238-dc2a4571b225",
-      isDeleted: false
-    }
-  ];
 
   constructor() {
     this.sequelize = new Sequelize(this.DataBaseName, this.UserName, this.Login, {
@@ -101,14 +56,11 @@ class UserModels {
   }
 
   public getAutoSuggestUsers(loginSubstring: string, limit: { from: number; to: number }): User[] {
-    return this.User.findAll({where:{login: { $like: `%${loginSubstring}%` }}, raw: true, offset: limit.from, limit: limit.to})
-      .then(users => console.log(users))
-      .catch(err => console.log(err));
+    console.log(Op.like);
 
-    return this.users
-      .filter(item => item.login.includes(loginSubstring))
-      .sort((a, b) => (a.login > b.login ? -1 : 1))
-      .slice(limit.from - 1, limit.to - 1);
+    return this.User.findAll({where: {login: {[Op.like]: `%${loginSubstring}%`}}, raw: true, offset: limit.from, limit: limit.to})
+      .then(users => console.log(1111, users))
+      .catch(err => console.log(err));
   }
 
   public addUser(user: User): void {
